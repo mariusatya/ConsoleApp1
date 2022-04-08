106a10bfcd807bd35c8b44ba9a9b132a0cbe04b0
Balázs <Balzs@DESKTOP-03D4AHH>
2022-04-08 16:49:31 +0200

    idk

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
 using System.IO;
-using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace SExperiment
 {
+    public class TravelRoute
+    {
+        public string honnan, hova;
+        public TravelRoute(string s)
+        {
+            this.honnan = "";
+            this.hova="";
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
+            string honnan = "";
+            string hova = "";
+            Console.WriteLine("Kérem adja meg honnan szeretne utazni: ");
+         honnan =  Console.ReadLine();
+            Console.WriteLine("Kérem adja meg hova szeretne utazni: ");
+            hova = Console.ReadLine();
+
             try
             {
                 string webAddr = "https://menetrendek.hu/menetrend/interface/index.php";
@@ -21,8 +38,7 @@ namespace SExperiment
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
 
-                    string json = "{ \"func\" : \"getRoutes\", \"params\" :{\"naptipus\":\"0\",\"datum\":\"2022-04-07\",\"honnan\":\"Mátészalka\",\"hova\":\"Nyírcsaholy\",\"hour\":\"0\",\"min\":\"0\",\"preferencia\":\"0\"}}";
-  
+                    string json = "{ \"func\" : \"getRoutes\", \"params\" :{\"naptipus\":\"0\",\"datum\":\"2022-04-08\",\"honnan\":\"Mátészalka\",\"hova\":\"Nyírcsaholy\",\"hour\":\"0\",\"min\":\"0\",\"preferencia\":\"0\"}}";                     
                     streamWriter.Write(json);
                     streamWriter.Flush();
                 }
@@ -31,7 +47,21 @@ namespace SExperiment
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
                     var responseText = streamReader.ReadToEnd();
-                    Console.WriteLine(responseText);
+                 //   Console.WriteLine(responseText);
+                    var data = (JObject)JsonConvert.DeserializeObject(responseText);
+                    for (int i = 1; i < 40; i++)
+                    {
+                        var indulasihely = data["results"]["talalatok"][$"{i}"]["indulasi_hely"];
+                        var erkezesihely = data["results"]["talalatok"][$"{i}"]["erkezesi_hely"];
+                        var erkezesiido = data["results"]["talalatok"][$"{i}"]["indulasi_ido"];
+                        var indulasiido = data["results"]["talalatok"][$"{i}"]["erkezesi_ido"];
+                        Console.WriteLine($"A busz/vonat {indulasiido}-kor indul {indulasihely}-ról");
+                        Console.WriteLine($"A busz/vonat {erkezesiido}-kor érkezik {erkezesihely}-ra");
+                    }
+                    
+
+                    
+
 
                     //Now you have your response.
                     //or false depending on information in the response     
@@ -40,7 +70,7 @@ namespace SExperiment
             catch (WebException ex)
             {
                 Console.WriteLine(ex.Message);
-                
+
             }
             Console.ReadKey();
         }
